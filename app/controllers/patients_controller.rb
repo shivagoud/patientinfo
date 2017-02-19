52c6a04b0093c7b514cd65d3d1968649f1555456
Shiva Goud <bshivagoud@gmail.com>
2017-02-19 06:58:48 +0530

    saving submitted patient info to db

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -7,4 +7,22 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.all
   end
+
+  def create
+    @patient = Patient.new(patient_params)
+    respond_to do |format|
+      if @patient.save
+        format.html { redirect_to root_path }
+        format.json { render :show, status: :created, location: @patient }
+      else
+        format.html { redirect_to root_path}
+        format.json { render json: @patient.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def patient_params
+    params.require(:patient).permit(:first_name, :last_name, :dob, :gender, :phone, :details)
+  end
 end
